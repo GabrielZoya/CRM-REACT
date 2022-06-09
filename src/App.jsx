a51ab2ad899cbb9b3b487a3c0b9e24fe47759157
Gabriel Zoya <zoyagabriel@gmail.com>
2022-06-09 16:54:15 -0300

    formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,6 @@
-import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import IniciarSesion from "./layout/iniciarSesion";
 import Layout from "./layout/Layout";
 import Inicio from "./pages/Inicio";
-import LoginForm from "./pages/LoginForm";
 import NuevoCliente from "./pages/NuevoCliente";
 import EditarCliente from "./pages/EditarCliente";
 
@@ -11,9 +8,6 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<IniciarSesion />}>
-          <Route index element={<LoginForm />} />
-        </Route>
         <Route path="/clientes" element={<Layout />}>
           <Route index element={<Inicio />} />
           <Route path="nuevo" element={<NuevoCliente />} />
