411cd817bd3ba3c73c78eecc28cc938ecdfd797e
Gabriel Zoya <zoyagabriel@gmail.com>
2022-06-24 12:21:18 -0300

    funcionalidad

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -17,6 +17,23 @@ const Inicio = () => {
     obtenerClientesApi();
   }, []);
 
+  const handleEliminar = async (id) => {
+    const confirmar = confirm("¿Deseas el iminar este cliente?");
+    if (confirmar) {
+      try {
+        const url = `http://localhost:4000/clientes/${id}`;
+        const respuesta = await fetch(url, {
+          method: "DELETE",
+        });
+
+        await respuesta.json();
+
+        const arrayClientes = clientes.filter((cliente) => cliente.id !== id);
+        setClientes(arrayClientes);
+      } catch (error) {}
+    }
+  };
+
   return (
     <>
       <h1 className="font-black text-4xl text-blue-900">Clientes</h1>
@@ -34,7 +51,11 @@ const Inicio = () => {
 
         <tbody>
           {clientes.map((cliente) => (
-            <Cliente key={cliente.id} cliente={cliente} />
+            <Cliente
+              key={cliente.id}
+              cliente={cliente}
+              handleEliminar={handleEliminar}
+            />
           ))}
         </tbody>
       </table>
